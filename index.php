03dc8aa99f43cb3b072cf248a7da7fe85785bea2
user002x23 <jhosuakenzo587@gmail.com>
2025-08-07 11:18:08 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,18 +1,14 @@
 <?php
 
-$cheatdll = '/cheat.cbm';
+$file_url = 'https://cheatku.vercel.app/cheat.cbm';
+$random_name = str_pad(mt_rand(1, 99999999), 8, '0', STR_PAD_LEFT);
 
-$randomfl = str_pad(mt_rand(1, 99999999), 8, '0', STR_PAD_LEFT);
-$original = pathinfo($cheatdll, PATHINFO_EXTENSION);
+header('Content-Type: application/octet-stream');
+header('Content-Disposition: attachment; filename="'.$random_name.'.cbm"');
 
-if(file_exists($cheatdll)) {
-    header('Content-Type: application/octet-stream');
-    header('Content-Disposition: attachment; filename="'.$randomfl.'.'.$original.'"');
-    readfile($cheatdll);
-    exit;
-} else {
-    die("File tidak ditemukan");
-}
-
+// Stream file dari URL
+readfile($file_url);
+exit;
 ?>
+
 
