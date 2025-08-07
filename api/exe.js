2f90a85fd0dd105ac1e00ebb5408bf6447849798
user002x23 <jhosuakenzo587@gmail.com>
2025-08-07 15:25:06 +0700

    Create exe.js

---
+++ api/exe.js
@@ -0,0 +1,12 @@
+const fs = require('fs');
+const path = require('path');
+
+module.exports = (req, res) => {
+  const filePath = path.join(__dirname, '../public/cheat.exe');
+  const randomName = `${Date.now()}.cbm`;
+  
+  res.setHeader('Content-Type', 'application/octet-stream');
+  res.setHeader('Content-Disposition', `attachment; filename="${randomName}"`);
+  
+  fs.createReadStream(filePath).pipe(res);
+};
